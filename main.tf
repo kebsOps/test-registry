a4c734f7ae3e6490ad69bf52bf427c0f3dd66353
kebsOps <kebeeyong@gmail.com>
2023-01-18 08:24:33 +0100

    changes

--- main.tf
+++ main.tf
@@ -1,11 +1,6 @@
-terraform {
-      required_providers {
-         aws = {
-         source = "hashicorp/aws"
-         version = "= 3.74.2"
-        }
-     }
-  }
+provider "aws" {
+  region = var.region
+}
 
 module "s3-webapp" {
   source  = "app.terraform.io/ACS-Kebs/s3-webapp/aws"
